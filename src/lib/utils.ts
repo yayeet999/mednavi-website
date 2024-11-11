e4f1cdcb282afbc0000614ebac262e995952bebe
yayeet999 <serenityai904@gmail.com>
2024-11-11 06:53:12 -0600

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+import { createContext, useContext } from "react"
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+type SelectContext = {
+  value: string | undefined
+  onValueChange: (value: string) => void
+}
+
+export const selectContext = createContext<SelectContext | undefined>(undefined)
