f59b598a1fbe8ab81cbeb18f2987fd4282dfbf22
yayeet999 <serenityai904@gmail.com>
2024-11-19 00:23:24 -0600

    Update tabs.tsx

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -1,8 +1,10 @@
 import * as React from "react"
 import * as TabsPrimitive from "@radix-ui/react-tabs"
 import { cn } from "@/lib/utils"
+
 const Tabs = TabsPrimitive.Root
-const TabsList = React.forwardRef
+
+const TabsList = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.List>,
   React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
 >(({ className, ...props }, ref) => (
@@ -16,7 +18,8 @@ const TabsList = React.forwardRef
   />
 ))
 TabsList.displayName = TabsPrimitive.List.displayName
-const TabsTrigger = React.forwardRef
+
+const TabsTrigger = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.Trigger>,
   React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
 >(({ className, ...props }, ref) => (
@@ -30,7 +33,8 @@ const TabsTrigger = React.forwardRef
   />
 ))
 TabsTrigger.displayName = TabsPrimitive.Trigger.displayName
-const TabsContent = React.forwardRef
+
+const TabsContent = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof TabsPrimitive.Content>
 >(({ className, ...props }, ref) => (
@@ -44,4 +48,5 @@ const TabsContent = React.forwardRef
   />
 ))
 TabsContent.displayName = TabsPrimitive.Content.displayName
+
 export { Tabs, TabsList, TabsTrigger, TabsContent }
