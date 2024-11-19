d9b5ccdb350380f765299707729b503896bc8493
yayeet999 <serenityai904@gmail.com>
2024-11-19 00:18:30 -0600

    Update tabs.tsx

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -1,5 +1,8 @@
+"use client"
+
 import * as React from "react"
 import * as TabsPrimitive from "@radix-ui/react-tabs"
+
 import { cn } from "@/lib/utils"
 
 const Tabs = TabsPrimitive.Root
@@ -11,7 +14,7 @@ const TabsList = React.forwardRef
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-10 items-center justify-center rounded-md p-1 text-muted-foreground",
+      "inline-flex h-10 items-center justify-center rounded-md text-muted-foreground", // Removed bg-muted here
       className
     )}
     {...props}
@@ -26,7 +29,7 @@ const TabsTrigger = React.forwardRef
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-1 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm",
+      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm",
       className
     )}
     {...props}
@@ -49,4 +52,9 @@ const TabsContent = React.forwardRef
 ))
 TabsContent.displayName = TabsPrimitive.Content.displayName
 
-export { Tabs, TabsList, TabsTrigger, TabsContent }
+export {
+  Tabs,
+  TabsList,
+  TabsTrigger,
+  TabsContent,
+}
