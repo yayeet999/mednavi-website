f49d69b7615ff8e28d484cca3c5ded9c90cff397
yayeet999 <serenityai904@gmail.com>
2024-11-18 22:58:55 -0600

    Update tabs.tsx

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -1,7 +1,5 @@
-// src/components/ui/tabs.tsx
 import * as React from "react"
 import * as TabsPrimitive from "@radix-ui/react-tabs"
-
 import { cn } from "@/lib/utils"
 
 const Tabs = TabsPrimitive.Root
