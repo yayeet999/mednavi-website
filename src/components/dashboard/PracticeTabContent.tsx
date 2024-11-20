166359c1067c4a95ba178cef374a8ecea6af2a0b
yayeet999 <serenityai904@gmail.com>
2024-11-20 03:34:53 -0600

    Update PracticeTabContent.tsx

--- src/components/dashboard/PracticeTabContent.tsx
+++ src/components/dashboard/PracticeTabContent.tsx
@@ -169,7 +169,7 @@ const DemographicsContent = () => {
           <h3 className="text-[8.5px] md:text-[13px] font-medium text-gray-700 mb-2">
             Age Distribution by Gender
           </h3>
-          <div className="h-[98px] md:h-[145px] w-full">
+          <div className="h-[98px] md:h-[141px] w-full">
             <ResponsiveContainer width="100%" height="100%">
               <BarChart
                 data={demographicsData}
@@ -227,13 +227,14 @@ const DemographicsContent = () => {
                 {window.innerWidth >= 768 && (
                   <Legend 
                     wrapperStyle={{
-                      fontSize: '10px',
+                      fontSize: '8px',
                       paddingTop: '-2px',
                       marginTop: '-4px',
                       textAlign: 'center',
                       width: '100%',
                       marginLeft: '6px',
                       marginRight: '6px',
+                      marginBottom: '2px',
                     }}
                     iconType="circle"
                     iconSize={4}
