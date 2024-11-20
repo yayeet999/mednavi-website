6e322444796afed6ab32a217311d8a211e41f5c0
yayeet999 <serenityai904@gmail.com>
2024-11-20 02:22:45 -0600

    Update PracticeTabContent.tsx

--- src/components/dashboard/PracticeTabContent.tsx
+++ src/components/dashboard/PracticeTabContent.tsx
@@ -215,7 +215,7 @@ const DemographicsContent = () => {
                     fontSize: window.innerWidth < 768 ? 6 : 8,
                     fill: '#4B5563'
                   }}
-                  padding={{ top: 5 }}
+                  padding={{ top: 0 }}
                 />
                 <Tooltip 
                   content={<CustomTooltip />}
