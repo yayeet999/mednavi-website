42101793aedfab46a43e40007f4400be843cb1d8
yayeet999 <serenityai904@gmail.com>
2024-11-20 03:16:09 -0600

    Update PracticeTabContent.tsx

--- src/components/dashboard/PracticeTabContent.tsx
+++ src/components/dashboard/PracticeTabContent.tsx
@@ -169,7 +169,7 @@ const DemographicsContent = () => {
           <h3 className="text-[8.5px] md:text-[13px] font-medium text-gray-700 mb-2">
             Age Distribution by Gender
           </h3>
-          <div className="h-[125px] md:h-[240px] w-full">
+          <div className="h-[115px] md:h-[220px] w-full">
             <ResponsiveContainer width="100%" height="100%">
               <BarChart
                 data={demographicsData}
