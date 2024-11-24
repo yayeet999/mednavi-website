af1683330f9af48691bb6f2d530b1884807869b5
yayeet999 <serenityai904@gmail.com>
2024-11-24 00:22:10 -0600

    Update FinancialsContent.tsx

--- src/components/dashboard/FinancialsContent.tsx
+++ src/components/dashboard/FinancialsContent.tsx
@@ -206,22 +206,22 @@ const FinancialsContent = () => {
               ))}
             </div>
             <div className="w-[65%] h-full flex items-center justify-center relative">
-              <<div className="absolute inset-0 flex items-center justify-center translate-y-[-8%] md:translate-y-0">
-  <div className="text-center">
-    <div className="text-[6.7px] md:text-xs text-gray-500">Total</div>
-    <div className="text-[8.5px] md:text-sm font-semibold text-gray-800">$79,355</div>
-  </div>
-</div>
-<ResponsiveContainer width="100%" height="100%">
-  <PieChart>
-    <Pie
-      data={expensesData}
-      innerRadius="60%"
-      outerRadius="90%"
-      paddingAngle={2}
-      dataKey="value"
-      className="animate-[rotate-pie_1.7s_ease-out] translate-y-[-8%] md:translate-y-0"
-    >
+              <div className="absolute inset-0 flex items-center justify-center translate-y-[-8%] md:translate-y-0">
+                <div className="text-center">
+                  <div className="text-[6.7px] md:text-xs text-gray-500">Total</div>
+                  <div className="text-[8.5px] md:text-sm font-semibold text-gray-800">$79,355</div>
+                </div>
+              </div>
+              <ResponsiveContainer width="100%" height="100%">
+                <PieChart>
+                  <Pie
+                    data={expensesData}
+                    innerRadius="60%"
+                    outerRadius="90%"
+                    paddingAngle={2}
+                    dataKey="value"
+                    className="animate-[rotate-pie_1.7s_ease-out] translate-y-[-8%] md:translate-y-0"
+                  >
                     {expensesData.map((entry, index) => (
                       <Cell key={`cell-${index}`} fill={entry.color} />
                     ))}
