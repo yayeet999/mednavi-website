4705a371c83b964564056582a05fd6917cfbeddb
yayeet999 <serenityai904@gmail.com>
2024-11-27 21:48:39 -0600

    Update AnalysisContent.tsx

--- src/components/dashboard/AnalysisContent.tsx
+++ src/components/dashboard/AnalysisContent.tsx
@@ -507,8 +507,8 @@ const AnalysisContent: React.FC<AnalysisContentProps> = ({
 
       {/* Financial Section */}
       {selectedIcon === 'financial' && selectedSubData === 'Avg Monthly Production' && (
-        <div className="grid grid-rows-2 gap-4 h-full">
-          <div className="bg-white rounded-lg p-2 md:p-5 shadow-sm w-full">
+        <div className="grid grid-rows-2 gap-3 h-full">
+          <div className="bg-white rounded-lg p-2 md:p-3 shadow-sm w-full">
             <MonthlyProductionChart
               data={data.financial.monthlyProduction.regional.breakdown}
               title="Regional Average"
@@ -516,7 +516,7 @@ const AnalysisContent: React.FC<AnalysisContentProps> = ({
               isDesktop={isDesktop}
             />
           </div>
-          <div className="bg-white rounded-lg p-2 md:p-5 shadow-sm w-full">
+          <div className="bg-white rounded-lg p-2 md:p-3 shadow-sm w-full">
             <MonthlyProductionChart
               data={data.financial.monthlyProduction.practice.breakdown}
               title="Your Practice"
